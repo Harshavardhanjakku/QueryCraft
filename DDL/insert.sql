b8c0784298707da3cae2e65e253f11bcd204748e
Harshavardhanjakku <2203a51012@sru.edu.in>
2026-04-09 01:11:10 +0530

    Added DCL , DML , DDL

---
+++ DDL/insert.sql
@@ -0,0 +1,5 @@
+INSERT INTO Batch03 VALUES ( 12 , "Harsha" , 21);
+INSERT INTO batch03 VALUES (17,"Shreya", 20);
+INSERT INTO batch03 VALUES (88,"Chaitra",20);
+INSERT INTO batch03 VALUES (85, "Akshith" , 22);
+
